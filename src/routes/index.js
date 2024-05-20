4fefaa646d21aca96c0dca97130c61c60ee3ba84
dung24102003 <21130321@st.hcmuaf.edu.vn>
2024-05-20 17:20:30 +0700

    1

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,17 @@
 import LogIn from "../pages/Login";
 import Home from "../pages/Home";
 import SignUp from "../pages/SignUp";
+import Information from "../pages/Information";
+import AccountProtection from "../pages/AccountProtection";
+import History from "../pages/HistoryLogin";
+
 const publicRoutes = [
   { path: "/home", component: Home },
   { path: "/", component: LogIn },
   { path: "/signup", component: SignUp },
+  { path: "/info", component:Information},
+  { path: "/acc_protect", component:AccountProtection},
+  { path: "/history", component:History},
 ];
 const privateRoutes = [];
 export { publicRoutes, privateRoutes };
