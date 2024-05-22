87cb73bfc4cbbbaeb446560d1d13dc57d357a8c5
DOXUANHAU <doxuanhau08022003@gmail.com>
2024-05-22 21:56:21 +0700

    checking master is ok

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,31 +1,25 @@
-
 import { useParams, useLocation } from "react-router-dom";
 import Menu from "./menu/menu";
 import List from "./list/list";
-import './home.css';
+import "./home.css";
 import Chat from "./chat/chat";
 import ChatList from "../data";
 import Friend from "./friend/friend";
-
-
 
 // function useQuery() {
 //   return new URLSearchParams(useLocation().search);
 // }
 
-const cx = classNames.bind(Styles);
 function Home() {
   // const query = useQuery();
   // const searchQuery = query.get("a");
   // console.log(typeof searchQuery);
   return (
-
-      <div className="container">
-          <Menu></Menu>
-         <List></List>
-          <Chat></Chat>
-        </div>
-
+    <div className="container">
+      <Menu></Menu>
+      <List></List>
+      <Chat></Chat>
+    </div>
   );
 }
 
