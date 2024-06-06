ce00f12a10247f3f54cce9cb400dc3b6bc128cfd
DOXUANHAU <doxuanhau08022003@gmail.com>
2024-06-06 18:23:12 +0700

    done get data feature

--- src/socket/WebsocketProvider.js
+++ src/socket/WebsocketProvider.js
@@ -1,7 +1,6 @@
-import { useState, useRef, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { WebsocketContext } from "./WebsocketContent";
 import useWebSocket from "react-use-websocket";
-// import { io } from "socket.io-client";
 
 function WebsocketProvider({ children }) {
   const [isReady, setIsReady] = useState(false);
@@ -19,26 +18,6 @@ function WebsocketProvider({ children }) {
 
   const ret = [isReady, val, sendJsonMessage];
 
-  // const [isReady, setIsReady] = useState(false);
-  // const [val, setVal] = useState(null);
-
-  // const ws = useRef(null);
-
-  // useEffect(() => {
-  //   const socket = new WebSocket("ws://140.238.54.136:8080/chat/chat");
-
-  //   socket.onopen = () => setIsReady(true);
-  //   socket.onclose = () => setIsReady(false);
-  //   socket.onmessage = (event) => setVal(event.data);
-
-  //   ws.current = socket;
-
-  //   return () => {
-  //     socket.close();
-  //   };
-  // }, []);
-
-  // const ret = [isReady, val, ws.current?.send.bind(ws.current)];
   return (
     <WebsocketContext.Provider value={ret}>
       {children}
@@ -47,39 +26,3 @@ function WebsocketProvider({ children }) {
 }
 
 export default WebsocketProvider;
-
-// function WebsocketProvider({ children }) {
-//   const [socket, setSocket] = useState(null);
-//   const [messages, setMessages] = useState([]);
-
-//   useEffect(() => {
-//     // Kết nối tới server WebSocket
-
-//     setSocket(newSocket);
-
-//     console.log(messages);
-//     // Nhận message từ server
-//     newSocket.on("message", (message) => {
-//       setMessages((prevMessages) => [...prevMessages, message]);
-//     });
-
-//     return () => {
-//       newSocket.close();
-//     };
-//   }, [setSocket]);
-
-//   const sendMessage = () => {
-//     if (socket) {
-//       socket.emit("message", "Hello from client!");
-//     }
-//   };
-//   sendMessage();
-//   const ret = [messages];
-
-//   return (
-//     <WebsocketContext.Provider value={ret}>
-//       {children}
-//     </WebsocketContext.Provider>
-//   );
-// }
-// export default WebsocketProvider;
