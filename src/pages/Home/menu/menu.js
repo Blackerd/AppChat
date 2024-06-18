76b8210ee338132db9b06aedfaa3cdae0c15b717
Blackerd <nguyenquyduong0802@gmail.com>
2024-06-18 13:03:47 +0700

    xu ly dang xuat

--- src/pages/Home/menu/menu.js
+++ src/pages/Home/menu/menu.js
@@ -1,48 +1,51 @@
-import ReactDOM from "react-dom";
+import React, { useContext } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faArrowRightFromBracket,
-  faGear,
-  faHouse,
-  faMessage,
-  faUser,
-} from "@fortawesome/free-solid-svg-icons";
+import { faArrowRightFromBracket, faGear, faMessage } from "@fortawesome/free-solid-svg-icons";
+import { Logout } from "../../../api/action";
+import { WebsocketContext } from "../../../socket/WebsocketContent";
 import "./menu.css";
 
 const Menu = (props) => {
+  const [, , sendJsonMessage] = useContext(WebsocketContext);
+
+  const handleLogout = () => {
+    const logoutAction = Logout(); // Tạo hành động đăng xuất sử dụng hàm Logout từ API
+    sendJsonMessage(logoutAction); // Gửi yêu cầu đăng xuất qua WebSocket
+
+    //  chuyển hướng về trang đăng nhập
+    window.location.href = '/';
+  };
+
   return (
-    <aside id="aside">
-      <div className="menu">
-        <div className="img">
-          <img
-            src="https://www.w3schools.com/howto/img_avatar.png"
-            alt="avatar"
-          />
-        </div>
+      <aside id="aside">
         <div className="menu">
-          <div className="menuItem">
-            <div className="item">
-              <div className="icon">
-                <FontAwesomeIcon icon={faMessage} />
+          <div className="img">
+            <img src="https://www.w3schools.com/howto/img_avatar.png" alt="avatar" />
+          </div>
+          <div className="menu">
+            <div className="menuItem">
+              <div className="item">
+                <div className="icon">
+                  <FontAwesomeIcon icon={faMessage} />
+                </div>
+                <span>{props.name}</span>
               </div>
-              <span>{props.name}</span>
-            </div>
-            <div className="item">
-              <div className="icon">
-                <FontAwesomeIcon icon={faGear} />
+              <div className="item">
+                <div className="icon">
+                  <FontAwesomeIcon icon={faGear} />
+                </div>
+                <span>Setting</span>
               </div>
-              <span>Setting</span>
-            </div>
-            <div className="logOut">
-              <div className="icon">
-                <FontAwesomeIcon icon={faArrowRightFromBracket} />
+              <div className="logOut" onClick={handleLogout}>
+                <div className="icon">
+                  <FontAwesomeIcon icon={faArrowRightFromBracket} />
+                </div>
+                <span>LogOut</span>
               </div>
-              <span>LogOut</span>
             </div>
           </div>
         </div>
-      </div>
-    </aside>
+      </aside>
   );
 };
 
